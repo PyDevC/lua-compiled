7ef1dffa6441ca61d498301dce270a9c8a580bb0
PyDevC <anuskcse@gmail.com>
2025-10-25 07:24:35 -0400

    Grammar for lua syntax, and some structures for forming ast
    For now we only have structures for now but next I will add some
    helper functions to parse each node.
    
    Also Simultaneously will add some helper functions in errors.c to
    collect errors and debug the code.

--- src/parser.h
+++ src/parser.h
@@ -1,50 +1,185 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#include "lexer.h"
-#include <string.h>
+/* Forward Declarations */
+typedef struct ExprNode ExprNode;
+typedef struct ExprNodeList ExprNodeList;
+typedef struct FieldList FieldList;
+typedef struct FuncBody FuncBody;
+typedef struct NameList NameList;
+typedef struct StatNode StatNode;
+typedef struct StatNodeList StatNodeList;
+typedef struct VarNode VarNode;
+typedef struct VarNodeList VarNodeList;
 
-typedef struct Expr {
+/* Grammar Structures */
+
+struct ExprNode {
   enum {
-    IntegerExpr,
+    NilExpr,
+    FalseExpr,
+    TrueExpr,
+    NumberExpr,
     StringExpr,
-    VariableExpr,
+    VargExpr,
+    FuncDecExpr,
+    PrefixExpr,
+    TableConstExpr,
     BinaryExpr,
-    UnaryExpr,
-    CallExpr,
+    UnaryExpr
   } type;
 
   union {
-    int integer_expr;
+    double number_expr;
     char *string_expr;
+    VarNode *varg_expr;
+    FuncBody *funcdec_expr;
+    FieldList *tableconst_expr;
+
     struct {
-      char *name;
-      struct Expr *value;
-    } variable_expr;
-    struct {
-      char opr;
-      struct Expr *left;
-      struct Expr *right;
+      char op;
+      ExprNode *left;
+      ExprNode *right;
     } binary_expr;
 
     struct {
-      char opr;
-      struct Expr *right;
+      char op;
+      ExprNode *left;
     } unary_expr;
+
+  } data;
+};
+
+struct ExprNodeList {
+  ExprNode *curr_expr;
+  ExprNodeList *next;
+};
+
+struct Var {
+  enum { NameVar, IndexVar, MemberVar } type;
+  union {
+    char *name;
+    struct {
+      ExprNode *prefix;
+      ExprNode *key;
+      bool is_index;
+    } complex_var;
+  } data;
+};
+
+struct VarList {
+  Var *var;
+  VarList *next;
+};
+
+struct FuncBody {
+  NameList *paralist;
+  StatNodeList *block;
+};
+
+struct Field {
+  enum { ArrayField, KeyValField, IndexField } type;
+  char *name;
+  ExprNode *Key;
+  ExprNode *value;
+};
+
+struct FieldList {
+  Field *field;
+  FieldList *next;
+};
+
+struct FuncCall {
+  enum { normal_call, member_call } type;
+  char *member_name;
+  ExprNodeList *arguments;
+  ExprNode *prefix;
+};
+
+struct StatNode {
+  enum {
+    AssignmentStat,
+    FuncCallStat,
+    DoBlockStat,
+    WhileBlockStat,
+    RepeatBlockStat,
+    IfBlockStat,
+    IteratorForBlockStat,
+    UnpackForBlockStat,
+    FunctionDefStat,
+    LocalFunctionDefStat,
+    LocalVarListDefStat,
+    ReturnStat,
+    BreakStat,
+    EmptyStat
+  } type;
+
+  union {
+
+    struct {
+      VarNodeList *varlist;
+      ExprNodeList *exprlist;
+    } assingment_stat;
+
+    FuncCall *funccall_stat;
+
+    struct {
+      ExprNode *condition;
+      StatNodeList *block;
+    } dolike_block_stat;
+
+    struct {
+      ExprNode *condition;
+      StatNodeList *block;
+      StatNode *next_clause;
+      bool is_else;
+    } if_else_stat;
 
     struct {
       char *name;
-      struct Expr_list *arguments;
-    } call_expr;
+      ExprNode *init;
+      ExprNode *final;
+      ExprNode *step;
+      StatNodeList *block;
+    } iteratorfor_stat;
 
-  } op;
-} AstNode;
+    struct {
+      NameList *namelist;
+      ExprNodeList *explist;
+      StatNodeList *block;
+    } unpackfor_stat;
 
-typedef struct Expr_list {
-  AstNode *elem;
-  struct Expr_list *next;
-} AstNodeList;
+    struct {
+      char *base_name;
+      char *method_name;
+      ExprNodeList *field_names;
+      FuncBody *body;
+    } functiondef_stat;
 
-AstNode *parse_token(TokenStruct);
+    struct {
+      char *name;
+      FuncBody *body;
+    } localfunctiondef_stat;
+
+    struct {
+      NameList *namelist;
+      ExprNodeList *explist;
+    } localvarlist_stat;
+
+    ExprNodeList *return_stat;
+
+  } data;
+};
+
+struct StatNodeList {
+  StatNode *stat;
+  StatNodeList *next;
+};
+
+/* Helper functions for parsing each grammar rule */
+
+StatNode *parse_chunk(StatNode *chunk); /* This is the main node that should be exposed */
+
+// will add more helper function to parse all the grammar non-terminals
 
 #endif // PARSER_H
